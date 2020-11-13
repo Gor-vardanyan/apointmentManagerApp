c1f2da3e052b01f441ef4e22375373ec7bb15e59
Gor <g.var.design@gmail.com>
2020-11-13 21:01:42 +0100

    cors

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const {tokenCheck} = require('./middleware/tokenchek');
 PORT = process.env.PORT || 5000;
 
 app.all((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://heroku-apointment-manager-app.herokuapp.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT,OPTIONS, DELETE");
+    req.header("Access-Control-Allow-Origin", "https://heroku-apointment-manager-app.herokuapp.com"); // update to match the domain you will make the request from
+    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    req.header("Access-Control-Allow-Methods", "GET, POST, PUT,OPTIONS, DELETE");
     next();
   });
 //El json lo parseamos mediante express para su uso
